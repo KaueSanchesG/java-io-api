2da2c19810b4c14716acd4472929eff652fe3ad9
KaueSanchesG <kauesanches123@gmail.com>
2025-06-25 13:29:33 -0300

    feat: list imp added

--- src/main/java/br/edu/utfpr/sistemarquivos/Command.java
+++ src/main/java/br/edu/utfpr/sistemarquivos/Command.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.sistemarquivos;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,7 +20,12 @@ public enum Command {
         Path execute(Path path) throws IOException {
 
             // TODO implementar conforme enunciado
-
+            try(DirectoryStream<Path> dir = Files.newDirectoryStream(path)){
+                System.out.println("Contents of " + path.toAbsolutePath());
+                for (Path p : dir){
+                    System.out.println(" " + p.getFileName());
+                }
+            }
             return path;
         }
     },
